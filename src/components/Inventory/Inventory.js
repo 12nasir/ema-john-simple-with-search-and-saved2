dc85c847441a91e829249260707a79e5b25a6c93
12nasir <nasirahmed8202@gmail.com>
2022-02-17 10:25:01 +0600

    something added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is my inventory page</h2>
+            <h2>This is my inventory page added</h2>
+            <h3>Welcome to my inventory</h3>
         </div>
     );
 };
