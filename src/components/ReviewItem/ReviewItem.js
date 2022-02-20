7483595819e53d272827964a4f54161269ba340a
12nasir <nasirahmed8202@gmail.com>
2022-02-20 09:56:38 +0600

    all added

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const ReviewItem = (props) => {
-    const {name, price, quantity, key} = props.products || {};
+    const {name, price, quantity, key} = props.product;
     const {handleRemove} = props;
     return (
         <div className='product'>
-            <div>
-                <h4 className='product-name'>Name: {name}</h4>
+           <div>
+                <h4 className='product-name'>{name}</h4>
                 <p>Price: {price}</p>
-                <p>Quantity: {quantity}</p>
-                <button onClick={ () => handleRemove (key)} className='btn-regular'>Remove</button>
-            </div>
+                <p>quantity: {quantity}</p>
+                <button onClick={()=> handleRemove(key)} className='btn-regular'>Remove</button>
+           </div>
         </div>
     );
 };
